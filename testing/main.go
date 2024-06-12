4675eb40283bd6821d6e8980a4073757162b5a37
yugoso8512 <yugoso8512@gmail.com>
2024-06-12 19:06:33 +0000

    ctrl s

--- testing/main.go
+++ testing/main.go
@@ -19,16 +19,16 @@ func UneficientFibonacci(n int) int {
 }
 
 func Efficientfibonacci(n int) int {
-    if n <= 1 {
-        return n
-    }
+	if n <= 1 {
+		return n
+	}
 
-    fib := make([]int, n+1)
-    fib[0], fib[1] = 0, 1
+	fib := make([]int, n+1)
+	fib[0], fib[1] = 0, 1
 
-    for i := 2; i <= n; i++ {
-        fib[i] = fib[i-1] + fib[i-2]
-    }
+	for i := 2; i <= n; i++ {
+		fib[i] = fib[i-1] + fib[i-2]
+	}
 
-    return fib[n]
+	return fib[n]
 }
