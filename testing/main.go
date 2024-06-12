5e63a7b74e2c5d7b49037d4c2b6ab7f8d704b83d
yugoso8512 <yugoso8512@gmail.com>
2024-06-12 16:54:27 +0000

    native testing with datatables

---
+++ testing/main.go
@@ -0,0 +1,12 @@
+package main
+
+func suma(x,y int) int {
+	return x + y
+}
+
+func GetMax(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
---
+++ testing/main_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+	/* 	total := suma(5, 5)
+
+	   	if total != 10 {
+	   		t.Errorf("Sum was incorrect, got %d, expected %d", total, 10)
+	   	}
+
+	*/
+
+	tables := []struct {
+		a int
+		b int
+		n int
+	}{
+		{1, 2, 3},
+		{2, 2, 4},
+		{25, 26, 51},
+	}
+
+	for _, item := range tables {
+		total := suma(item.a, item.b)
+		if total != item.n {
+			t.Errorf("Sum was wrong, expected %d, got %d", item.n, total)
+		}
+	}
+
+}
+
+func TestGetMax(t *testing.T){
+	if GetMax(12, 15) != 15 {
+		t.Errorf("GetMax was incorrect, got %d, expected %d", GetMax(12, 15), 15)
+	}
+}
