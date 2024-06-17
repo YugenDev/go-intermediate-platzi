2a23c79dd5cdd290300a397f378f7ec424b6f08b
yugoso8512 <yugoso8512@gmail.com>
2024-06-17 01:53:40 +0000

    channels de lectura y escritura

--- concurrency/pipelines/main.go
+++ concurrency/pipelines/main.go
@@ -1,2 +1,36 @@
 package main
 
+import "fmt"
+
+func Generator(c chan<- int) {
+	for i := 1; i <= 10; i++ {
+		c <- i
+	}
+	close(c)
+}
+
+func Double(in <-chan int, out chan<- int) {
+	for value := range in {
+		out <- 2 * value
+	}
+
+	close(out)
+}
+
+func PrintValues(c chan int) {
+	for value := range c {
+		fmt.Println(value)
+	}
+}
+
+func main(){
+	c := make(chan int)
+	d := make(chan int)
+
+	go Generator(c)
+	go Double(c, d)
+	println(d)
+	println(c)
+	PrintValues(d)
+	
+}
